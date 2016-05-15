dc6cfc1519c00e725d10edbe7366294f9cd4b6bd
Joseph <andson.josef@gmail.com>
2016-05-15 02:06:52 -0300

    done?

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-  $("#slider").owlCarousel({
- 
-    
+$("#slider").owlCarousel({   
       slideSpeed : 300,
       paginationSpeed : 400,
       singleItem:true
- 
-  });
- 
+});
+$("ul.navbar-nav > li > a").on('click', function(event){
+	event.preventDefault();
+	$("html, body").animate({scrollTop:$(this.hash).offset().top}, 1200);
+})
