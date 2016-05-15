0037089a5d7e64dafd1f8c467de750a977fb6db0
Joseph <andson.josef@gmail.com>
2016-05-15 04:00:51 -0300

    done!

--- js/main.js
+++ js/main.js
@@ -5,5 +5,17 @@ $("#slider").owlCarousel({
 });
 $("ul.navbar-nav > li > a").on('click', function(event){
 	event.preventDefault();
-	$("html, body").animate({scrollTop:$(this.hash).offset().top}, 1200);
+
+	$('ul.navbar-nav > li').removeClass('active');
+	$(this).parent().addClass('active');
+	$("html, body").animate({scrollTop:$(this.hash).offset().top-50}, 1200);
+});
+
+$(window).scroll(function(){
+	var scrollatual = $(this).scrollTop();
+	if (scrollatual > 200){
+		$('#navegacao').addClass('fixed');
+	}else{
+		$('#navegacao').removeClass('fixed');
+	}
 })
